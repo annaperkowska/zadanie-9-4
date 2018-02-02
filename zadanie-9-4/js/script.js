d618247d7745cffae49edb762220900e18fe750b
Anna Perkowska <perkowska.a@gmail.com>
2018-02-02 22:51:52 +0100

    CHange script

--- zadanie-9-4/js/script.js
+++ zadanie-9-4/js/script.js
@@ -1,13 +1,11 @@
-var rows = prompt("Ile poziomów ma mieć choinka?");
-
 function drawTree(rows) {
-	for ( var i = 0 ; i < rows ; i++) {
-		var star = "";
-		for ( var j = 0 ; j <= i ; j++) {
+	for (var i = 0 ; i <= rows ; i++) {
+		var star = " ";
+		for (var j = 0 ; j <= i ; j++) {
 			star += "* ";
 		}
 		console.log(star);
 	}
 }
-
+drawTree(prompt("Ile poziomów ma mieć choinka. Podaj liczbę"));
 
