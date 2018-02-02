a4c3347bfe15a6a28f5332346b4df53d2964c028
Anna Perkowska <perkowska.a@gmail.com>
2018-02-02 23:43:52 +0100

    change script.js

--- zadanie-9-4/js/script.js
+++ zadanie-9-4/js/script.js
@@ -1,11 +1,13 @@
 function drawTree(rows) {
-	for (var i = 0 ; i <= rows ; i++) {
-		var star = " ";
-		for (var j = 0 ; j <= i ; j++) {
-			star += "* ";
+	for (var i = 0 ; i < rows ; i++) {
+		var star = "";
+		for (var j = 0; j <= rows - i; j++) {
+			star += " ";
+		}
+		for (var k = 0 ; k <= i ; k++) {
+			star += "* "
 		}
 		console.log(star);
 	}
 }
-drawTree(prompt("Ile poziomów ma mieć choinka. Podaj liczbę"));
-
+drawTree(prompt("Ile poziomów ma mieć choinka. Podaj liczbę")); 
